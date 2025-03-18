a17effe6c3c35c3b478b890e8b7fb48fcfd1ba43
juanfranfdezGit <jfranromfdez@gmail.com>
2025-03-18 09:25:36 +0100

    images fixed, hamburguer animation
    feat: fixed images sizes from slider and add animation to hamburguer menu

--- js/hamburguer.js
+++ js/hamburguer.js
@@ -2,14 +2,24 @@ export default function hamburguer() {
     const header = document.getElementsByClassName("header")[0];
     const headerNav = document.getElementsByClassName("header__nav--mobile")[0];
     const hamburguer = document.getElementsByClassName("header__hamburguer")[0];
+    const spans = hamburguer.querySelectorAll("span");
 
     hamburguer.addEventListener("click", () => {
         if (headerNav.className === "header__nav--mobile disallow") {
             headerNav.className = "header__nav--mobile";
             header.className = "header disallow";
+
+            spans.forEach((span) => {
+                span.className = "hamburguer__allow";
+            })
+
         } else if (headerNav.className === "header__nav--mobile") {
             headerNav.className = "header__nav--mobile disallow";
             header.className = "header";
+
+            spans.forEach((span) => {
+                span.className = "";
+            })
         }
     })
 }
